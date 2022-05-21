9fb4e9e26f609298680b3a71cba3f91d3a128c60
Chawput <chawput@gmail.com>
2022-05-22 00:03:51 +0700

    ep26 State

--- client/src/components/FormComponent.js
+++ client/src/components/FormComponent.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormComponent = () => {
+  const [state, setState] = useState({
+    title: '',
+    content: '',
+    author: '',
+  });
+
+  const { title, content, author } = state;
+
   return (
     <div className="container p-5">
+      <pre>{ JSON.stringify(state)}</pre>
       <h1>เขียนบทความ</h1>
       <form action="">
         <div className="form-group">
           <label htmlFor="">ชื่อบทความ</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" value={title} />
         </div>
         <div className="form-group">
           <label htmlFor="">บทความ</label>
-          <textarea className="form-control" name="" id="" cols="30" rows="10"></textarea>
+          <textarea className="form-control" value={content}></textarea>
         </div>
         <div className="form-group">
           <label htmlFor="">ผู้เขียน</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" value={author}/>
         </div>
         <br/>
         <input type="submit" value="บันทึก" className='btn btn-primary' />
