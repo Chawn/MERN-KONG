397ba93207fea69bd325a7bd8bbfa1063955a650
Chawput <chawput@gmail.com>
2022-05-22 00:15:03 +0700

    ep27 State, setState, Event

--- client/src/components/FormComponent.js
+++ client/src/components/FormComponent.js
@@ -1,36 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const FormComponent = () => {
-  const [state, setState] = useState({
-    title: '',
-    content: '',
-    author: '',
-  });
+	const [state, setState] = useState({
+		title: '',
+		content: '',
+		author: '',
+	});
 
-  const { title, content, author } = state;
+	const { title, content, author } = state;
 
-  return (
-    <div className="container p-5">
-      <pre>{ JSON.stringify(state)}</pre>
-      <h1>เขียนบทความ</h1>
-      <form action="">
-        <div className="form-group">
-          <label htmlFor="">ชื่อบทความ</label>
-          <input type="text" className="form-control" value={title} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="">บทความ</label>
-          <textarea className="form-control" value={content}></textarea>
-        </div>
-        <div className="form-group">
-          <label htmlFor="">ผู้เขียน</label>
-          <input type="text" className="form-control" value={author}/>
-        </div>
-        <br/>
-        <input type="submit" value="บันทึก" className='btn btn-primary' />
-      </form>
-    </div>
-  );
-}
+	// กำหนดค่าให้ State
+	const inputValue=name=>event=> {
+    setState({...state, [name]:event.target.value});
+	};
 
-export default FormComponent
+	return (
+		<div className='container p-5'>
+			<pre>{JSON.stringify(state)}</pre>
+			<h1>เขียนบทความ</h1>
+			<form action=''>
+				<div className='form-group'>
+					<label htmlFor=''>ชื่อบทความ</label>
+					<input
+						type='text'
+						className='form-control'
+						value={title}
+						onChange={inputValue('title')}
+					/>
+				</div>
+				<div className='form-group'>
+					<label htmlFor=''>บทความ</label>
+					<textarea
+						className='form-control'
+						value={content}
+						onChange={inputValue('content')}
+					></textarea>
+				</div>
+				<div className='form-group'>
+					<label htmlFor=''>ผู้เขียน</label>
+					<input
+						type='text'
+						className='form-control'
+						value={author}
+						onChange={inputValue('author')}
+					/>
+				</div>
+				<br />
+				<input type='submit' value='บันทึก' className='btn btn-primary' />
+			</form>
+		</div>
+	);
+};
+
+export default FormComponent;
