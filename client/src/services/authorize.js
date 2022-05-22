924735eff4cd2511a75ad1d2694f84ac5bec9407
Chawput <chawput@gmail.com>
2022-05-23 00:44:03 +0700

    ep60. ดึงข้อมูล Token & Username

--- client/src/services/authorize.js
+++ client/src/services/authorize.js
@@ -10,3 +10,25 @@ export const authenticate = (response, next) => {
   next();
   
 }
+
+// ดึงข้อมูล token
+export const getToken = () => {
+  if(window !== 'undefined'){
+    if(sessionStorage.getItem('token')){
+      return JSON.parse(sessionStorage.getItem('token'));
+    }else{
+      return false;
+    }
+  }
+}
+
+// ดึงข้อมูล username
+export const getUsername = () => {
+  if(window !== 'undefined'){
+    if(sessionStorage.getItem('username')){
+      return JSON.parse(sessionStorage.getItem('username'));
+    }else{
+      return false;
+    }
+  }
+}
