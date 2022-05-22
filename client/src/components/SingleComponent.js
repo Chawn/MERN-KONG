13b667466972ba07e137ec021d67d73e48264bbc
Chawput <chawput@gmail.com>
2022-05-22 12:17:04 +0700

    41. แสดงข้อมูลใน Component (Client)

--- client/src/components/SingleComponent.js
+++ client/src/components/SingleComponent.js
@@ -1,14 +1,31 @@
+import axios from 'axios'
 import React from 'react'
 import NavbarComponent from './NavbarComponent'
+import { useState, useEffect } from 'react'
 
 const SingleComponent = (props) => {
+  const [blog, setBlog] = useState('');
+  
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API}/blog/${props.match.params.slug}`)
+    .then(res => {
+      setBlog(res.data.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
+
+
   return (
     <div className='container p-5'>
       <NavbarComponent />
-
-      <div>SingleComponent</div>
-      {props.match.params.slug}
-      <pre>{ JSON.stringify(props,null, 2)}</pre>
+      <h2>{blog.title}</h2>
+      <div class="py-4">{ blog.content }</div>
+      <p>ผู้เขียน: {blog.author}, เผยแพร่ {new Date(blog.createdAt).toLocaleString()}</p>
+      <code>
+        <pre>{ JSON.stringify(blog, null, 2)}</pre>
+      </code>
     </div>
   )
 }
